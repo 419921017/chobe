865d1ea58f848323c03b051e54523e1d9dfcc893
power_840 <liuyu19921017@gmail.com>
2023-11-29 22:30:23 +0800

    feat: sidebar default active

--- src/page/common/sider/index.js
+++ src/page/common/sider/index.js
@@ -1,43 +1,51 @@
-import { helper } from 'utils';
-import { menus } from 'utils/menu';
-var templateLeft = require('./index.template');
+import { helper } from "utils";
+import { menus } from "utils/menu";
+var templateLeft = require("./index.template");
 
 const pageFn = {
   init: function () {
     const url = window.location.pathname;
     const patter = /^\/(.*).html/;
-    let name = patter.exec(url) ? patter.exec(url)[1] : 'index';
-    const type = Number(helper.getUrlParam("type")) || 1;
+    let name = patter.exec(url) ? patter.exec(url)[1] : "index";
+    let type = Number(helper.getUrlParam("type")) || 1;
+    if (name === "news") type = 4;
     const urlTemp = {
       delivery_detail: "delivery",
       news_detail: "news",
-      culture_detail:"culture",
-      fund_detail:"fund",
-    }
+      culture_detail: "culture",
+      fund_detail: "fund",
+    };
     name = urlTemp[name] ? urlTemp[name] : name;
     // if (!menu_item) {
     //   return
     // }
-    const menu_item = menus.find(i => i.key === name);
-    const list = menu_item.children.map(i => ({
+    const menu_item = menus.find((i) => i.key === name);
+    const list = menu_item.children.map((i) => ({
       name: i.name,
       enName: i.enName,
-      className: type === i.id ? "on" : '',
+      className: type === i.id ? "on" : "",
       path: i.path,
     }));
-    const result = helper.renderHtml(templateLeft, { list, name: menu_item.name, enName: menu_item.enName });
-    const $leftMenus = $('#leftMenus')
+    const result = helper.renderHtml(templateLeft, {
+      list,
+      name: menu_item.name,
+      enName: menu_item.enName,
+    });
+    const $leftMenus = $("#leftMenus");
     $leftMenus.html(result);
-  }
-}
+  },
+};
 
 $(function () {
   pageFn.init();
   $(window).scroll(function () {
-    if ($(window).scrollTop() >= $(".top1").height() + $(".c_banner").height()) {
-      $(".erji").addClass("on")
+    if (
+      $(window).scrollTop() >=
+      $(".top1").height() + $(".c_banner").height()
+    ) {
+      $(".erji").addClass("on");
     } else {
-      $(".erji").removeClass("on")
+      $(".erji").removeClass("on");
     }
   });
-})
+});
